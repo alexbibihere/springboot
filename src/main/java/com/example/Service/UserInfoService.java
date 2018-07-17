d742d2764988616347f9b2c49cea2bfd3e7fa677
329731984@qq.com <329731984@qq.com>
2018-07-18 00:53:40 +0800

    springboot + shiro 整合

---
+++ src/main/java/com/example/Service/UserInfoService.java
@@ -0,0 +1,9 @@
+package com.example.Service;
+
+
+import com.example.pojo.UserInfo;
+
+public interface UserInfoService {
+    /**通过username查找用户信息;*/
+    public UserInfo findByUsername(String username);
+}
--- src/main/java/com/example/pojo/UserInfo.java
+++ src/main/java/com/example/pojo/UserInfo.java
@@ -1,17 +1,16 @@
 package com.example.pojo;
 
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
 /**
  * @author: yan
- * @Date: 2018/4/16/0016 2:37
+ * @Date: 2018/7/18/0018 0:12
  * @Description:
  */
 @Entity
-public class User implements Serializable{
+public class UserInfo implements Serializable {
     @Id
     @GeneratedValue
     private Integer uid;
@@ -23,8 +22,7 @@ public class User implements Serializable{
     private byte state;//用户状态,0:创建未认证（比如没有激活，没有输入验证码等等）--等待验证的用户 , 1:正常状态,2：用户被锁定.
     @ManyToMany(fetch= FetchType.EAGER)//立即从数据库中进行加载数据;
     @JoinTable(name = "SysUserRole", joinColumns = { @JoinColumn(name = "uid") }, inverseJoinColumns ={@JoinColumn(name = "roleId") })
-    private List<SysRole> sysRoleList;// 一个用户具有多个角色
-
+    private List<SysRole> roleList;// 一个用户具有多个角色
 
     public Integer getUid() {
         return uid;
@@ -74,11 +72,20 @@ public class User implements Serializable{
         this.state = state;
     }
 
-    public List<SysRole> getSysRoleList() {
-        return sysRoleList;
+    public List<SysRole> getRoleList() {
+        return roleList;
     }
 
-    public void setSysRoleList(List<SysRole> sysRoleList) {
-        this.sysRoleList = sysRoleList;
+    public void setRoleList(List<SysRole> roleList) {
+        this.roleList = roleList;
     }
+
+    /**
+     * 密码盐.
+     * @return
+     */
+    public String getCredentialsSalt(){
+        return this.username+this.salt;
+    }
+    //重新对盐重新进行了定义，用户名+salt，这样就更加不容易被破解
 }
